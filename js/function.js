57b48c99e9b5051c82e12ce9864db7cef2b9d56b
AdaRemes <aremes24@students.oamk.fi>
2026-09-22 14:10:29 +0300

    toinen versio jossa vastaukset

---
+++ js/function.js
@@ -0,0 +1,11 @@
+
+function showAnswer(number) {
+    const answer = document.getElementById("answer" + number);
+
+    if (answer.style.display === "block") {
+        answer.style.display = "none";
+    } else {
+        answer.style.display = "block";
+    }
+}
+
